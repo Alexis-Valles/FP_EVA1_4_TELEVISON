ba4a19ec03809ef243cea4ac55dbb211075322f2
Alexis-Valles <L24550808@chihuahua2.tecnm.mx>
2024-08-29 16:07:14 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_television;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //DECLARAR VARIABLES
+        String marca;
+        String modelo;
+        int tama;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Samsung";
+        modelo = "CU7010";
+        tama = 70;
+        precio = 35000;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(tama);
+        System.out.println(precio);
+    }
+    
+}
